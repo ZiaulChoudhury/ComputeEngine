521ad5584b9fef53b5a7be8c9fbd0aaafef23be4
Ziaul <ziaul.1987@gmail.com>
2021-06-17 18:56:43 +0530

    Both 3x3 and 5x5 tested on the new design

--- generate.cpp
+++ generate.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-#define K 3
+#define K 5
 int main()
 {
 	for(int i=0;i<8; i++)
 		for(int j=0;j<8;j++){
 			if(i<K && j<K){
-				printf("%d ", (i*8+j)*4);
+				printf("%d ", (i*8+j));
 			}
 		}
 }
